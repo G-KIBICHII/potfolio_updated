2a7c70fb02d7bb7a68f42f5a038a83752d4b1de9
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-09-17 22:31:02 +0300

    message

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,34 +12,46 @@ const About = () => {
 
         <div className="about-section">
           <div className="about-left">
-            <img width="300px" src={profile} alt="" />
+            <img width="100%" src={profile} alt="" />
           </div>
           <div className="about-right">
             <div className="about-para">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem quaerat cumque sed soluta rerum dolorem esse sunt
-                dolore repellendus nesciunt.
+              Hi, I’m Gilbert, a versatile developer with expertise in Next.js, Angular, React.js, React Native, Vue.js, Nuxt.js, Node.js, and Django. I specialize in crafting dynamic, high-performance web and mobile applications by leveraging these cutting-edge technologies.My focus is on delivering seamless user experiences and scalable solutions, whether I’m building server-rendered sites, cross-platform apps, or robust back-end systems.
               </p>
               <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae,
-                vero saepe magnam assumenda error repellat corrupti? Quidem
-                dolorem veritatis quisquam.
+              Driven by a passion for technology and problem-solving, I’m always exploring new tools and approaches to enhance my development skills. I thrive on innovation and collaboration, and I’m eager to connect with others to turn ideas into reality. If you’re interested in discussing a project or exploring new opportunities, let’s get in touch!
               </p>
             </div>
             <div className="about-skills">
               <div className="about-skill">
-                <p>HTML & CSS </p> <hr style={{ width: "50%" }} />
+                <p>React.js </p> <hr style={{ width: "100%" }} />
               </div>
               <div className="about-skill">
-                <p>React </p> <hr style={{ width: "70%" }} />
+                <p>React Native</p> <hr style={{ width: "70%" }} />
               </div>
               <div className="about-skill">
-                <p>Next</p> <hr style={{ width: "60%" }} />
+                <p>Angular </p> <hr style={{ width: "70%" }} />
               </div>
               <div className="about-skill">
+                <p>Next.js</p> <hr style={{ width: "60%" }} />
+              </div>
+              <div className="about-skill">
+                <p>Vue.js</p> <hr style={{ width: "50%" }} />
+              </div>
+              {/* <div className="about-skill">
                 <p>Javascript</p> <hr style={{ width: "50%" }} />
+              </div> */}
+              <div className="about-skill">
+                <p>Django</p> <hr style={{ width: "70%" }} />
               </div>
+              <div className="about-skill">
+                <p>Nuxt.js</p> <hr style={{ width: "50%" }} />
+              </div>
+              <div className="about-skill">
+                <p>Node.js</p> <hr style={{ width: "100%" }} />
+              </div>
+
             </div>
             <div className="about-achievements">
               <div className="about-achievement">
